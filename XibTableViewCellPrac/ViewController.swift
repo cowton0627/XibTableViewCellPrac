482fb5816655057a408819650f5a6cf02565dade
Charles Cheng <charles_cheng@microjet.com.tw>
2023-08-03 14:00:32 +0800

    Initial Commit

---
+++ XibTableViewCellPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XibTableViewCellPrac
+//
+//  Created by Chun-Li Cheng on 2023/8/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
